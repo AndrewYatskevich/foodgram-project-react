304f859ee0478ceea425838d7ce5f8f6aaa29d3b
Andrew Yatskevich <drew.yatskevich@gmail.com>
2022-11-09 14:36:29 +0300

    fix pep warnings

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,6 +1,5 @@
 from colorfield.fields import ColorField
 from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator
 from django.db import models
 
 User = get_user_model()
